bd9ff980c3d600455286b7a73b358c8d1c210a11
lejusdefruits <julienbrs1er@gmail.com>
2026-06-05 23:31:49 +0200

    end and start structures added

--- notlonggraph/constants.py
+++ notlonggraph/constants.py
@@ -1,1 +1,25 @@
-# Constantes sentinelles (START, END).
+class Sentinel:
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return self.name
+
+class _END(Sentinel):
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, "_instance"):
+            cls._instance = super(_END, cls).__new__(cls)
+        return cls._instance
+    def __init__(self):
+        super().__init__("END")
+
+class _START(Sentinel):
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, "_instance"):
+            cls._instance = super(_START, cls).__new__(cls)
+        return cls._instance
+    def __init__(self):
+        super().__init__("START")
+
+END = _END()
+START = _START()
