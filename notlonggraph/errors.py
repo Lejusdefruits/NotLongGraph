d1954d8a7a63691bb04754eb3fb892bc24b3578c
lejusdefruits <julienbrs1er@gmail.com>
2026-06-05 23:51:57 +0200

    bases doc for errors

--- notlonggraph/errors.py
+++ notlonggraph/errors.py
@@ -1,1 +1,7 @@
-# Exceptions maison du framework.
+class NotLongGraphError(Exception):
+    """raised when the graph is invalid"""
+    pass
+
+class GraphError(NotLongGraphError):
+    """base class for graph exceptions"""
+    pass
